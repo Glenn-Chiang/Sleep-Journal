0e7ed3e9cef801fa2f8b44c1370a5cbfe1d3724d
Glenn Chiang <glennchiang24@gmail.com>
2023-11-23 22:37:06 +0800

    Refined JournalEntry grid layout responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-screen h-16 px-4 bg-sky-500 shadow text-white flex items-center">
+    <nav className="fixed top-0 left-0 w-screen h-16 px-4 bg-sky-500 shadow-lg text-white flex items-center">
       <Link href={"/"}>
         <h1 className="flex gap-2 items-center">
           <FontAwesomeIcon icon={faMoon} />
--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { calculateAverageTime, calculateDuration } from "@/lib/dateTime";
-import { faBed } from "@fortawesome/free-solid-svg-icons";
+import { faBed, faChartArea, faChartBar, faPieChart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Entry } from "@prisma/client";
 import { convertDurationToHoursAndMinutes, formatTime } from "../lib/dateTime";
@@ -23,12 +23,18 @@ export const Summary = ({ entries }: SummaryProps) => {
     .filter((entry) => !!entry.wakeTime) // Only consider completed entries
     .map((entry) => calculateDuration(entry.sleepTime, entry.wakeTime));
 
-  const averageSleepDuration = convertDurationToHoursAndMinutes(sleepDurations.reduce((sum, value) => sum + value, 0) / sleepDurations.length)
+  const averageSleepDuration = convertDurationToHoursAndMinutes(
+    sleepDurations.reduce((sum, value) => sum + value, 0) /
+      sleepDurations.length
+  );
 
   return (
     <section className="flex flex-col gap-4 w-full">
-      <h1 >Your Sleep Summary</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-slate-500 bg-white rounded-xl p-4 w-full">
+      <h1 className="flex gap-2 items-center">
+        <FontAwesomeIcon icon={faChartBar} />
+        Summary
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 text-slate-500 bg-white rounded-xl p-4 w-full shadow">
         <div className="flex flex-col gap-4 text-black">
           <h2>Average sleep duration</h2>
           <div className="flex gap-4 items-center text-sky-500">
@@ -42,19 +48,18 @@ export const Summary = ({ entries }: SummaryProps) => {
             )}
           </div>
         </div>
-        <div className="flex flex-col justify-between">
-          <div className="flex gap-2 items-center">
-            <span>Average time you go to bed</span>
-            <span className="text-sky-500">
-              {entries.length ? formatTime(averageSleepTime) : "-"}
-            </span>
-          </div>
-          <div className="flex gap-2 items-center">
-            <span>Average time you wake up</span>
-            <span className="text-sky-500">
-              {entries.length ? formatTime(averageWakeTime) : "-"}
-            </span>
-          </div>
+
+        <div className="flex gap-2 items-center sm:flex-col">
+          <span className="text-center">Average time you go to bed</span>
+          <span className="text-sky-500 sm:text-2xl">
+            {entries.length ? formatTime(averageSleepTime) : "-"}
+          </span>
+        </div>
+        <div className="flex gap-2 items-center sm:flex-col">
+          <span className="text-center">Average time you wake up</span>
+          <span className="text-sky-500 sm:text-2xl">
+            {entries.length ? formatTime(averageWakeTime) : "-"}
+          </span>
         </div>
       </div>
     </section>
