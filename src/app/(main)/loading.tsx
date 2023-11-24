327d7569a2e7d6c7c07b221d50edb7e8d0ce0d05
Glenn Chiang <glennchiang24@gmail.com>
2023-11-24 23:35:24 +0800

    Fixed loading page

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -1,7 +1,7 @@
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const LoadingIndicator = () => {
+export default function LoadingPage() {
   return (
     <div className="text-sky-500 flex gap-4 justify-center items-center text-2xl w-full h-screen fixed top-0 m-auto">
       <FontAwesomeIcon icon={faSpinner} className="animate-spin" />
