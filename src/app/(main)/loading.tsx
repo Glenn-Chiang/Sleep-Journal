a647dc1d478f971a78bc87b22d624497846b3f99
Glenn Chiang <glennchiang24@gmail.com>
2023-11-24 23:24:52 +0800

    Modified LoadingIndicator

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -1,11 +1,11 @@
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Loading() {
+export const LoadingIndicator = () => {
   return (
-    <div className="flex flex-col">
-      <FontAwesomeIcon icon={faSpinner} className="animate-spin"/>
+    <div className="text-sky-500 flex gap-4 justify-center items-center text-2xl w-full h-screen fixed top-0 m-auto">
+      <FontAwesomeIcon icon={faSpinner} className="animate-spin" />
       Loading...
     </div>
-  )
-}
+  );
+};
