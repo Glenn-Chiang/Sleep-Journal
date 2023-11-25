1f86e49e040c183d69a373f9af8b4bf11f0c3d3f
Glenn Chiang <glennchiang24@gmail.com>
2023-11-25 20:40:43 +0800

    Added avatar to UserPage

--- src/app/(main)/admin/user/[id]/page.tsx
+++ src/app/(main)/admin/user/[id]/page.tsx
@@ -3,19 +3,37 @@ import { getUser } from "@/actions/users";
 import { EntriesList } from "@/components/EntriesList";
 import { Summary } from "@/components/Summary";
 import { TopButton } from "@/components/buttons";
-import { faBook } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBook,
+  faChevronLeft,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function UserPage({ params }: { params: { id: string } }) {
   const userId = Number(params.id);
   const user = await getUser(userId);
-  const entries = await getUserEntries(userId)
+  const entries = await getUserEntries(userId);
 
   return (
     <>
-      <h1>
-        <span className="text-sky-500">{user?.username}&apos;s</span> Journal
-      </h1>
+      <Link
+        href={"/admin"}
+        className="flex gap-2 items-center text-sky-500 self-start text-xl"
+      >
+        <FontAwesomeIcon icon={faChevronLeft} />
+        Users
+      </Link>
+      <div className="flex gap-4 items-center">
+        {user?.avatarUrl && (
+          <Image src={user?.avatarUrl} alt="" width={80} height={80} className="rounded-full"/>
+        )}
+        <h1 className="text-start">
+          {user?.username}
+        </h1>
+      </div>
       <Summary entries={entries} />
       <section className="w-full flex flex-col gap-4">
         <h1 className="text-start flex gap-2 items-center">
